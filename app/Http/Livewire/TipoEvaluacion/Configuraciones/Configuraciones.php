41c9ec7f830acac5cb070db75ffd2829fdd6483e
josuechevezUEES <josue.chevez@uees.edu.sv>
2024-03-20 13:49:42 -0600

    crear componente configuraciones

---
+++ app/Http/Livewire/TipoEvaluacion/Configuraciones/Configuraciones.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\TipoEvaluacion\Configuraciones;
+
+use Livewire\Component;
+
+class Configuraciones extends Component
+{
+    public function render()
+    {
+        return view('livewire.tipo-evaluacion.configuraciones.configuraciones');
+    }
+}
