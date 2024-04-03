887129aa2da233c5f415ec2e6ce05e954bc44491
josuechevezUEES <josue.chevez@uees.edu.sv>
2024-04-03 13:49:41 -0600

    fix home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\EvaEvaluacione;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -24,5 +25,20 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    public function obtener_evaluaciones($estudiante)
+    {
+        $buscar_evaluaciones_activa = EvaEvaluacione::whereHas('tiposEvaluacione.tpeConfiguracion.configuracionEntidades', function ($query) {
+            $query->where('evaluador_id', 3);
+        })
+            ->whereHas('tiposEvaluacione.tpeConfiguracion.configuracionFacultades', function ($query) use ($estudiante) {
+                $query->where('codigo_facultad', $estudiante->CARCOD);
+            })
+            ->whereHas('tiposEvaluacione.tpeConfiguracion.configuracionModalidades', function ($query) use ($estudiante) {
+                $query->where('modalidad', $estudiante->MODALIDAD);
+            })
+            ->where('estado', 1)
+            ->get();
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,13 +10,11 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">
-                        <h5><span class="text-center fa fa-home"></span> @yield('title')</h5>
-                    </div>
-                    <div class="card-body">
-                        <h5>Hi <strong>{{ Auth::user()->name }},</strong>
-                            {{ __('You are logged in to ') }}{{ config('app.name', 'Laravel') }}</h5>
-                        </br>
+                    <div class="card-header bg-navy">
+                        <h5>
+                            <span class="text-center fa fa-home"></span>
+                            Evaluaciones
+                        </h5>
                     </div>
                 </div>
             </div>
