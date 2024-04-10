f43fbd21a446d01bc6157764e8eb748e5df68bcd
josuechevezUEES <josue.chevez@uees.edu.sv>
2024-04-10 13:14:32 -0600

    fix component

--- resources/views/livewire/estudiantes/evaluaciones/evaluaciones.blade.php
+++ resources/views/livewire/estudiantes/evaluaciones/evaluaciones.blade.php
@@ -1,3 +1,37 @@
-<div>
-    {{-- Do your work, then step back. --}}
+<div class="row" wire:poll>
+    @forelse ($evaluaciones as $evaluacion)
+        <div
+            class="col-sm-{{ $evaluaciones->count() > 1 ? 12 : 12 }} col-lg-12 col-md-{{ $evaluaciones->count() > 1 ? 6 : 6 }}">
+            <div class="card">
+                <div class="card-header bg-navy">
+                    <strong>
+                        {{ $evaluacion->tiposEvaluacione->nombre }}
+                    </strong>
+                </div>
+                <div class="card-body">
+                    <p>
+                        <strong>Periodo Evaluacion</strong>
+                        <br>
+                        {{ $evaluacion->fecha_inicio_evaluacion ? date('d-m-Y h:i A', strtotime($evaluacion->fecha_inicio_evaluacion)) : null }}
+                        -
+                        {{ $evaluacion->fecha_fin_evaluacion ? date('d-m-Y h:i A', strtotime($evaluacion->fecha_fin_evaluacion)) : null }}
+
+                    </p>
+                    <p>
+                        <strong>Descripcion:</strong>
+                        <br>
+                        {{ $evaluacion->tiposEvaluacione->descripcion }}
+                    </p>
+                    @if ($evaluacion->disponible == true)
+                        <a href="#{{ $evaluacion->id }}">Completar Evaluacion</a>
+                    @else
+                        <a href="#{{ $evaluacion->id }}" class="text-danger">
+                            No disponible, periodo finalizado
+                        </a>
+                    @endif
+                </div>
+            </div>
+        </div>
+    @empty
+    @endforelse
 </div>
--- resources/views/livewire/estudiantes/evaluaciones/index.blade.php
+++ resources/views/livewire/estudiantes/evaluaciones/index.blade.php
@@ -1,9 +1,21 @@
-@extends('layouts.app')
+@extends('adminlte::page')
 
-@section('title')
-    Evaluaciones
-@endsection
+@section('title', 'Evaluaciones')
+
+@section('content_header')
+    {{ Breadcrumbs::render('instrumentos_evaluaciones.index') }}
+@stop
 
 @section('content')
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <livewire:estudiantes.evaluaciones.evaluaciones />
+            </div>
+        </div>
+    </div>
+@endsection
 
+@section('footer')
+    Universidad Evangelica de El Salvador | {{ date('Y') }}
 @endsection
