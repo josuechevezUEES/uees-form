2148847ceeca63d5043e5ba3fa08b54b4b09e8d5
josueUEES <josue.chevez@uees.edu.sv>
2024-04-22 13:14:39 -0600

    fix users

--- app/Models/User.php
+++ app/Models/User.php
@@ -63,4 +63,18 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    public function getRememberToken()
+    {
+        return $this->remember_token;
+    }
+
+    public function setRememberToken($value)
+    {
+        $this->remember_token = $value;
+    }
+
+    public function getRememberTokenName()
+    {
+        return 'remember_token';
+    }
 }
