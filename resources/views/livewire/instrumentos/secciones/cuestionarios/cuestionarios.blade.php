629be67ed071ff363232c06f7ad280db7e8edbb7
josueUEES <josue.chevez@uees.edu.sv>
2024-05-02 15:03:16 -0600

    feat: create component's

--- resources/views/livewire/instrumentos/secciones/cuestionarios/cuestionarios.blade.php
+++ resources/views/livewire/instrumentos/secciones/cuestionarios/cuestionarios.blade.php
@@ -6,89 +6,87 @@
 
 <div class="container-fluid">
     @include('livewire.instrumentos.secciones.cuestionarios.modal')
-    <form action="">
-        <div class="row justify-content-center">
-            <div class="col-sm-10">
-                <div class="card">
-                    <div class="card-header bg-navy">
-                        <div class="row">
-                            <div class="col-sm-10">
-                                <h2>
-                                    {{ $seccion->literal }}. Instrumento
-                                </h2>
-                            </div>
-                            <div class="col-sm-2">
-                                <button type="button" wire:click='crear()' class="btn bg-warning btn-block"
-                                    data-bs-toggle="modal" data-bs-target="#createDataModal">
-                                    Nueva
-                                </button>
-                            </div>
+    <div class="row justify-content-center">
+        <div class="col-sm-10">
+            <div class="card">
+                <div class="card-header bg-navy">
+                    <div class="row">
+                        <div class="col-sm-10">
+                            <h2>
+                                {{ $seccion->literal }}. Instrumento
+                            </h2>
+                        </div>
+                        <div class="col-sm-2">
+                            <button type="button" wire:click='crear()' class="btn bg-warning btn-block"
+                                data-bs-toggle="modal" data-bs-target="#createDataModal">
+                                Nueva
+                            </button>
                         </div>
                     </div>
-                    <div class="card-body">
-                        <div class="row p-4 gap-5">
-                            @forelse ($preguntas_instrumento as $pregunta)
-                                <div class="col-sm-12">
-                                    <div class="px-3 w-100">
-                                        @if ($pregunta->tipo_pregunta_id == 1)
-                                            <livewire:instrumentos.secciones.cuestionarios.preguntas.pregunta-cerrada
-                                                :pregunta="$pregunta" :seccion="$seccion" :wire:key="$loop->iteration"/>
-                                        @endif
+                </div>
+                <div class="card-body">
+                    <div class="row p-4 gap-5">
+                        @forelse ($preguntas_instrumento as $pregunta)
+                            <div class="col-sm-12">
+                                <div class="px-3 w-100">
+                                    @if ($pregunta->tipo_pregunta_id == 1)
+                                        <livewire:instrumentos.secciones.cuestionarios.preguntas.pregunta-cerrada
+                                            :pregunta="$pregunta" :seccion="$seccion" wire:key="'pregunta-'.$pregunta->id" />
+                                    @endif
 
-                                        @if ($pregunta->tipo_pregunta_id == 2)
-                                            <div class="mb-4">
-                                                <h3>
-                                                    {{ $seccion->literal }}.{{ $pregunta->sub_numeral }})
-                                                    {{ $pregunta->nombre }}
-                                                </h3>
-                                                <div class="row pt-3">
-                                                    @if (isset($pregunta->opciones))
-                                                        @forelse ($pregunta->opciones as $opcion)
-                                                            @if ($loop->iteration == 1)
-                                                                <div class="col-sm-12">
-                                                                    <input type="{{ $opcion['entrada'] }}"
-                                                                        name="{{ $seccion->literal }}.{{ $pregunta->sub_numeral }}"
-                                                                        {{ $pregunta->requerido ? 'required' : null }}
-                                                                        class="form-control form-control-border border-width-2"
-                                                                        placeholder="{{ $opcion['nombre'] }}">
-                                                                </div>
-                                                            @else
-                                                                <div class="col-sm-12">
-                                                                    <input type="{{ $opcion['entrada'] }}"
-                                                                        name="{{ $seccion->literal }}.{{ $pregunta->sub_numeral }}"
-                                                                        {{ $pregunta->requerido ? 'required' : null }}
-                                                                        class="form-control form-control-border border-width-2"
-                                                                        placeholder="{{ $opcion['nombre'] }}">
-                                                                </div>
-                                                            @endif
-                                                        @empty
-                                                        @endforelse
-                                                    @endif
-                                                </div>
+                                    @if ($pregunta->tipo_pregunta_id == 2)
+                                        <div class="mb-4">
+                                            <h3>
+                                                {{ $seccion->literal }}.{{ $pregunta->sub_numeral }})
+                                                {{ $pregunta->nombre }}
+                                            </h3>
+                                            <div class="row pt-3">
+                                                @if (isset($pregunta->opciones))
+                                                    @forelse ($pregunta->opciones as $opcion)
+                                                        @if ($loop->iteration == 1)
+                                                            <div class="col-sm-12">
+                                                                <input type="{{ $opcion['entrada'] }}"
+                                                                    name="{{ $seccion->literal }}.{{ $pregunta->sub_numeral }}"
+                                                                    {{ $pregunta->requerido ? 'required' : null }}
+                                                                    class="form-control form-control-border border-width-2"
+                                                                    placeholder="{{ $opcion['nombre'] }}">
+                                                            </div>
+                                                        @else
+                                                            <div class="col-sm-12">
+                                                                <input type="{{ $opcion['entrada'] }}"
+                                                                    name="{{ $seccion->literal }}.{{ $pregunta->sub_numeral }}"
+                                                                    {{ $pregunta->requerido ? 'required' : null }}
+                                                                    class="form-control form-control-border border-width-2"
+                                                                    placeholder="{{ $opcion['nombre'] }}">
+                                                            </div>
+                                                        @endif
+                                                    @empty
+                                                    @endforelse
+                                                @endif
                                             </div>
-                                        @endif
+                                        </div>
+                                    @endif
 
-                                        @if ($pregunta->tipo_pregunta_id == 3)
-                                            <livewire:instrumentos.secciones.cuestionarios.preguntas.pregunta-seleccion-multiple
-                                                :pregunta="$pregunta" :seccion="$seccion" :wire:key="$loop->iteration"/>
-                                        @endif
+                                    @if ($pregunta->tipo_pregunta_id == 3)
+                                        <livewire:instrumentos.secciones.cuestionarios.preguntas.pregunta-seleccion-multiple
+                                            :pregunta="$pregunta" :seccion="$seccion" wire:key="'pregunta-'.$pregunta->id" />
+                                    @endif
 
-                                        @if ($pregunta->tipo_pregunta_id == 4)
-                                            <livewire:instrumentos.secciones.cuestionarios.preguntas.pregunta-cerrada-compleja
-                                                :pregunta="$pregunta" :seccion="$seccion" :wire:key="$loop->iteration"/>
-                                        @endif
-                                    </div>
-
+                                    @if ($pregunta->tipo_pregunta_id == 4)
+                                        <livewire:instrumentos.secciones.cuestionarios.preguntas.pregunta-cerrada-compleja
+                                            :pregunta="$pregunta" :seccion="$seccion" wire:key="'pregunta-'.$pregunta->id" />
+                                    @endif
                                 </div>
-                            @empty
-                                <div class="col-sm-12">
-                                    @include('adminLTE.errors.datos-no-encontrados')
-                                </div>
-                            @endforelse
-                        </div>
+
+                            </div>
+                        @empty
+                            <div class="col-sm-12">
+                                @include('adminLTE.errors.datos-no-encontrados')
+                            </div>
+                        @endforelse
                     </div>
                 </div>
             </div>
         </div>
-    </form>
+    </div>
 </div>
--- resources/views/livewire/instrumentos/secciones/cuestionarios/preguntas/pregunta-seleccion-multiple.blade.php
+++ resources/views/livewire/instrumentos/secciones/cuestionarios/preguntas/pregunta-seleccion-multiple.blade.php
@@ -1,42 +1,108 @@
 <div class="mb-4">
-    <h3>
-        {{ $seccion->literal }}.{{ $pregunta->sub_numeral }})
-        {{ $pregunta->nombre }}
-    </h3>
+    <div class="row mb-3">
+        @if ($activar_edicion == true)
+            <div class="col-sm-12">
+                <div class="input-group">
+                    <input type="text" name="nombre_pregunta" id="nombre_pregunta" placeholder="Nueva Preguntas"
+                        class="form-control form-control-border" wire:model='nombre_pregunta'>
+
+                    <div class="input-group-append">
+                        <button type="button" class="btn btn-outline-dark" wire:click='desactivar_edicion'>
+                            <i class="fas fa-save"></i>
+                            <div wire:loading.delay wire:target='desactivar_edicion'>
+                                <div class="spinner-border spinner-border-sm" role="status">
+                                    <span class="sr-only">Loading...</span>
+                                </div>
+                            </div>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        @else
+            <div class="col-sm-12">
+                <h3>
+                    {{ $seccion->literal }}.{{ $pregunta->sub_numeral }})
+                    {{ $pregunta->nombre }}
+
+                    <button type="button" class="btn btn-sm btn-outline-dark" wire:click='activar_edicion'>
+                        <i class="fas fa-pencil-alt"></i>
+                        <div wire:loading.delay wire:target='activar_edicion'>
+                            <div class="spinner-border spinner-border-sm" role="status">
+                                <span class="sr-only">Loading...</span>
+                            </div>
+                        </div>
+                    </button>
+                    <button type="button" class="btn btn-sm btn-outline-dark"
+                        wire:click="mostrar_formulario_nueva_opcion">
+                        Agregar Opcion
+                        <div wire:loading.delay wire:target='mostrar_formulario_nueva_opcion'>
+                            <div class="spinner-border spinner-border-sm" role="status">
+                                <span class="sr-only">Loading...</span>
+                            </div>
+                        </div>
+                    </button>
+                </h3>
+            </div>
+        @endif
+    </div>
+
     @if (isset($pregunta->opciones))
-        <div class="row pt-3">
+        <div class="row">
             @forelse ($pregunta->opciones as $opcion)
                 @if ($loop->iteration == 1)
                     <div class="col-sm-auto">
-                        <div class="form-group form-check">
-                            <input type="{{ $opcion['entrada'] }}" class="form-check-input"
-                                name="{{ $seccion->literal }}.{{ $pregunta->sub_numeral }}"
-                                wire:model="opciones_seleccionada" 
-                                value="{{ $opcion['nombre'] }}"
-                                {{ $pregunta->requerido ? 'required' : null }}
-                                id="check{{ $opcion['nombre'] }}">
-                            <label class="form-check-label" for="check{{ $opcion['nombre'] }}">
-                                {{ $opcion['nombre'] }}
-                            </label>
-                        </div>
+                        <livewire:instrumentos.secciones.cuestionarios.preguntas.pregunta-seleccion-multiple-opcion
+                            :seccion="$seccion" :pregunta="$pregunta" :opcion="$opcion" wire:key="{{ $opcion->id.time() }}" />
                     </div>
                 @else
-                    <div class="col-sm-auto ml-md-3">
-                        <div class="form-group form-check">
-                            <input type="{{ $opcion['entrada'] }}" class="form-check-input"
-                                name="{{ $seccion->literal }}.{{ $pregunta->sub_numeral }}"
-                                wire:model="opciones_seleccionada"
-                                value="{{ $opcion['nombre'] }}" 
-                                {{ $pregunta->requerido ? 'required' : null }}
-                                id="check{{ $opcion['nombre'] }}">
-                            <label class="form-check-label" for="check{{ $opcion['nombre'] }}">
-                                {{ $opcion['nombre'] }}
-                            </label>
-                        </div>
+                    <div class="col-sm-auto">
+                        <livewire:instrumentos.secciones.cuestionarios.preguntas.pregunta-seleccion-multiple-opcion
+                            :seccion="$seccion" :pregunta="$pregunta" :opcion="$opcion" wire:key="{{ $opcion->id.time() }}" />
                     </div>
                 @endif
             @empty
             @endforelse
+        </div>
+    @endif
+
+    @if ($mostrar_formulario)
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="input-group">
+                    <div class="input-group-append">
+                        <button type="button" class="btn btn-sm btn-outline-dark" wire:click='almacenar_opcion()'
+                            wire:target="cancelar_almacenar_opcion,nuevo_nombre_opcion"
+                            wire:loading.attr="cancelar_almacenar_opcion">
+                            <i class="fas fa-save"></i>
+                            <div wire:loading.delay wire:target='almacenar_opcion'>
+                                <div class="spinner-border spinner-border-sm" role="status">
+                                    <span class="sr-only">Loading...</span>
+                                </div>
+                            </div>
+                        </button>
+                    </div>
+
+                    <input type="text" name="nuevo_nombre_opcion" id="nuevo_nombre_opcion"
+                        wire:model='nuevo_nombre_opcion' placeholder="Nueva opcion"
+                        class="form-control form-control-sm border border-bottom border-dark">
+
+                    <div class="input-group-append">
+                        <button type="button" class="btn btn-sm btn-outline-dark"
+                            wire:click='cancelar_almacenar_opcion()' wire:target="almacenar_opcion,nuevo_nombre_opcion"
+                            wire:loading.attr="almacenar_opcion">
+                            <i class="fas fa-times-circle"></i>
+                            <div wire:loading.delay wire:target='cancelar_almacenar_opcion'>
+                                <div class="spinner-border spinner-border-sm" role="status">
+                                    <span class="sr-only">Loading...</span>
+                                </div>
+                            </div>
+                        </button>
+                    </div>
+                </div>
+                @error('nuevo_nombre_opcion')
+                    <span class="error text-danger">{{ $message }}</span>
+                @enderror
+            </div>
         </div>
     @endif
 </div>
