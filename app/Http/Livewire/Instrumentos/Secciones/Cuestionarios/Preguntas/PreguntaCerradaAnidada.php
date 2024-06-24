4694ed60a57c9e1e4fa5979e3bbc15b4911a8be0
josueUEES <josue.chevez@uees.edu.sv>
2024-06-24 15:58:49 -0600

    fix app

--- app/Http/Livewire/Instrumentos/Secciones/Cuestionarios/Preguntas/PreguntaCerradaAnidada.php
+++ app/Http/Livewire/Instrumentos/Secciones/Cuestionarios/Preguntas/PreguntaCerradaAnidada.php
@@ -2,6 +2,11 @@
 
 namespace App\Http\Livewire\Instrumentos\Secciones\Cuestionarios\Preguntas;
 
+/**
+ * Pregunta cerrada que permite a las opciones
+ * realizar acciones o comportamiento unico
+ */
+
 use Livewire\Component;
 
 class PreguntaCerradaAnidada extends Component
